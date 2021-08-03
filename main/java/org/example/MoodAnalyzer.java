17db258f2dd8a1c257a29d4212a6dd2e4e6f12b5
rohitdighe19 <rohitdighe47@gmail.com>
2021-08-04 00:01:17 +0530

    Added to solution of use-case-2

--- main/java/org/example/MoodAnalyzer.java
+++ main/java/org/example/MoodAnalyzer.java
@@ -1,12 +1,35 @@
 package org.example;
 
 class MoodAnalyser {
+    private String message;
 
-    public String analyseMood(String message) {
-        if(message.contains("sad"))
-            return "SAD";
-        else
-            return "HAPPY";
+    public MoodAnalyser() {
     }
 
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    /**
+     * Purpose : To check message and return accordingly
+     *           - Handles Exceptions using try catch block
+     *
+     * @return sad if message contains sad else false
+     */
+    public String analyseMood() {
+
+        try {
+
+            if (message.contains("sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+
+        } catch (Exception e) {
+
+            return "HAPPY";
+
+        }
+
+    }
 }
--- test/java/org/example/MoodAnalyserTest.java
+++ test/java/org/example/MoodAnalyserTest.java
@@ -6,18 +6,27 @@ import org.junit.Test;
 
 public class MoodAnalyserTest {
 
+    // Returns SAD if message is "I am in sad mood"
     @Test
     public void givenMessage_ToMoodAnalyser_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String message = moodAnalyser.analyseMood("I am in sad mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
+        String message = moodAnalyser.analyseMood();
         Assert.assertThat(message, CoreMatchers.is("SAD"));
     }
 
+    // Returns Happy if message is "I am in any mood"
     @Test
     public void givenMessage_ToMoodAnalyser_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String message = moodAnalyser.analyseMood("I am in any mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any mood");
+        String message = moodAnalyser.analyseMood();
         Assert.assertThat(message,CoreMatchers.is("HAPPY"));
     }
 
+    // Handles Exception
+    @Test
+    public void givenMessageNull_ToMoodAnalyser_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("");
+        String message = moodAnalyser.analyseMood();
+        Assert.assertThat(message,CoreMatchers.is("HAPPY"));
+    }
 }
